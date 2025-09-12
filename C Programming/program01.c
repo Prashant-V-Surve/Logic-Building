e47362fff05ffb603ae97f80be4bd8c8b1d54f7a
Prashant-V-Surve <prashantsurve110@gmail.com>
2025-09-12 15:05:30 +0530

    Update program01.c

--- C Programming/program01.c
+++ C Programming/program01.c
@@ -1,3 +1,12 @@
+///////////////////////////////////////////////////////
+//
+//  File name :     program1.c
+//  Descreption :   Hello to the World of C
+//  Author :        Prashant V. Surve
+//  Date :          28/04/2025
+//
+///////////////////////////////////////////////////////
+
 #include<stdio.h>
 
 int main()
