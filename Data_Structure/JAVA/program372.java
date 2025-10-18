27e05b36b855167fc5839065de80aee98e777bc3
Prashant-V-Surve <prashantsurve110@gmail.com>
2025-10-19 00:05:21 +0530

    Add files via upload

---
+++ Data_Structure/JAVA/program372.java
@@ -0,0 +1,36 @@
+//////////////////////////////////////////////////////////////////////////////
+//     
+//  File name :     program372.java
+//  Descreption :   Linked List(Singly Linear).
+//  Author :        Prashant V. Surve
+//  Date :          7/07/2025
+//
+//////////////////////////////////////////////////////////////////////////////
+
+class node
+{
+    public int no;
+    public node next;
+}
+
+class SinglyLL
+{
+    private node first;
+    private int iCount;
+
+    public SinglyLL()
+    {
+        System.out.println("Inside constructor");
+        first = null;
+        iCount = 0;
+    }
+}
+
+class program372
+{
+    public static void main(String A[])
+    {
+        SinglyLL sobj = new SinglyLL();
+
+    }
+}
